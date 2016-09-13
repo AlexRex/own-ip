8ce047488cfcb87202f05dd8bb7626827fb897e2
Torres Mateu, Alejandro <Alejandro.TorresMateu@adidas-group.com>
2016-09-13 12:24:52 +0200

    init commit

---
+++ ownIp.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+var os = require('os'),
+ifaces = os.networkInterfaces(),
+ncp = require('copy-paste');
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+      ncp.copy(iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+      ncp.copy(iface.address);
+    }
+    ++alias;
+  });
+});
